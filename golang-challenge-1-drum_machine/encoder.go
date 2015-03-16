ae50a1657c8c5c2708c8617016296e3d30288c99
Abhinav <abhinav.kaushik@gmail.com>
2015-03-16 16:57:00 +0530

    adding the encoder

---
+++ golang-challenge-1-drum_machine/encoder.go
@@ -0,0 +1,26 @@
+package drum
+
+import (
+    _ "fmt"
+    "io/ioutil"
+)
+
+
+func Encode(pat *Pattern) (data []byte, err error) {
+    var e encodeState
+    e.init(pat)
+    data, err = e.encode()
+    return
+}
+
+func EncodeToFile(path string, pat *Pattern) (err error) {
+    var e encodeState
+    e.init(pat)
+    data, err := e.encode()
+
+    if (err != nil)  {
+        return
+    }
+    err = ioutil.WriteFile(path, data, 0644)
+    return
+}
